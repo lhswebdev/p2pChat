772aece89e521829765d7ec478a8128ac5234669
Saggins <win32prog@gmail.com>
2021-10-18 17:55:24 -0700

    OkayyyImdoine

--- src/ChatWindow.svelte
+++ src/ChatWindow.svelte
@@ -1,19 +1,7 @@
 <script>
     import { peer, conn } from 'store.js';
-    $: allChats = []
+    $: messages = []
     let msg;
-    
-    // conn.on('open', function() {
-    //   // Receive messages
-    //   conn.on('data', function(data) {
-    //     console.log('Received', data);
-            
-    //   });
-
-    //   // Send messages
-    //   conn.send('Hello!');
-    // });
-    //https://svelte.dev/tutorial/auto-subscriptions
     let conn_value;
     conn.subscribe(value => {
         conn_value = value;
@@ -21,6 +9,7 @@
     
     function sendMsg() {
         conn_value.send(msg)
+        messages = [...messages, msg]
     }
 </script>
 <!--
@@ -30,7 +19,10 @@
     <p>{chat.msg}</p>
 endforeach
     https://svelte.dev/tutorial/each-block-bindings -->
+    {#each messages as peerMessage}
+        <p>{peerMessage}</p>
 
+    {/each}
     <input type="text" bind:value={msg}>
     <button on:click={sendMsg(msg)}>Send</button>
     
